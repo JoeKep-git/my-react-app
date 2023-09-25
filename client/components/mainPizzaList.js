787557807493c6d7b12c890d9de2ec3f11783a03
JoeKep-git <joeykeputabusiness@gmail.com>
2023-09-25 19:07:13 +0100

    updating packages. Displaying pizzas that are saved in nodejs

--- client/components/mainPizzaList.js
+++ client/components/mainPizzaList.js
@@ -14,42 +14,19 @@ const PizzaList = () => {
     return (
         <>
             <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                {pizzas.map((pizza) => (
+                    <div key={pizza.id} className="col">
+                        <div className="card">
+                            <img src={pizza.imageSrc} className="card-img-top" alt={pizza.name} />
+                            <div className="card-body">
+                                <h5 className="card-title">{pizza.name}</h5>
+                                <p className="card-text">Toppings: {pizza.toppings.join(', ')}</p>
+                                <p className="card-text">Size: {pizza.size}</p>
+                                <p className="card-text">Price: ${pizza.price}</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </div> 
+                ))}
             </div>
         </>
     );
