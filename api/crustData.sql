2c648723dfa61e00d93dd39902341324fb705e29
JoeK <ohhjassassin@gmail.com>
2023-09-27 17:30:42 +0100

    adding sql file for crust data

---
+++ api/crustData.sql
@@ -0,0 +1,8 @@
+INSERT INTO pizzaCrust("pizzaCrust") VALUES
+('Thin Italian'),
+('Stone Crust'),
+('Cheese Stuffed Crust'),
+('Vegan'),
+('Gluten-Free')
+select * from pizzaCrust
+DBCC CHECKIDENT ('pizzaCrust', RESEED, 1);
